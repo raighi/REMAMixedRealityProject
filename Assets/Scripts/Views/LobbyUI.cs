2247641952e8c9c7e4486c4c56672a032b78be0a
raphael hierso iglesias <raphael.hierso-iglesias@imt-atlantique>
2026-03-08 11:00:19 +0100

    Modification Lobbys + Modification Build Profile XR (ajout de scene list particulière) + Modification prefab bouton et UI

--- Assets/Scripts/Views/LobbyUI.cs
+++ Assets/Scripts/Views/LobbyUI.cs
@@ -1,28 +1,48 @@
-// Views/LobbyUI.cs
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LobbyUI : MonoBehaviour
 {
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
-    [SerializeField] private string hostIP = "192.168.1.X"; // IP de la machine (vérifier avec ipconfig)
+    [SerializeField] private TMP_InputField ipField;
+
+    private const string VR_SCENE = "LobbyVR";
+    private const string AR_SCENE = "LobbyAR";
 
     void Start()
     {
-        hostButton.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartHost();
-            gameObject.SetActive(false);
-        });
+        hostButton.onClick.AddListener(OnHostClicked);
+        clientButton.onClick.AddListener(OnClientClicked);
+    }
 
-        clientButton.onClick.AddListener(() => {
-            // Configure l'IP avant de rejoindre
-            var transport = NetworkManager.Singleton
-                .GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
-            transport.SetConnectionData(hostIP, 7777);
-            NetworkManager.Singleton.StartClient();
-            gameObject.SetActive(false);
-        });
+    private void OnHostClicked()
+    {
+        NetworkManager.Singleton.StartHost();
+        LoadGameScene();
+    }
+
+    private void OnClientClicked()
+    {
+        var transport = NetworkManager.Singleton
+            .GetComponent<UnityTransport>();
+        transport.SetConnectionData(ipField.text, 7777);
+        NetworkManager.Singleton.StartClient();
+        // Le client attend que le serveur charge la scène
+    }
+
+    private void LoadGameScene()
+    {
+        bool isAR = SystemInfo.deviceType == DeviceType.Handheld;
+        string sceneName = isAR ? AR_SCENE : VR_SCENE;
+
+        NetworkManager.Singleton.SceneManager.LoadScene(
+            sceneName,
+            LoadSceneMode.Single
+        );
     }
 }
