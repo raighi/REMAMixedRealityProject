a4623cb0397baa2031b7431f0cfea69fd9597808
Scarfinos <augustinfaure6@gmail.com>
2026-03-20 16:34:01 +0100

    Adding Intro text options

---
+++ Assets/Scripts/AugustinTest/IntroBootstrapper.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroBootstrapper : MonoBehaviour
+{
+    private IntroController introController;
+    private TextIntroModel textIntroModel;
+
+    [Header("Text Data")]
+    public TextSequence textSequence;
+
+    [Header("View")]
+    public BackgroundIntroView backgroundIntroView;
+
+    [Header("Interaction")]
+    public IntroInteractions introInteractions;
+
+    private void Start()
+    {
+        textIntroModel = new TextIntroModel(textSequence);
+        introController = new IntroController(textIntroModel, backgroundIntroView);
+        introInteractions.Initialize(introController);
+    }
+}
---
+++ Assets/Scripts/AugustinTest/IntroController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class IntroController
+{
+    private BackgroundIntroView backgroundIntroView;
+    private TextIntroModel textIntroModel;
+
+    public IntroController(TextIntroModel model, BackgroundIntroView view)
+    {
+        textIntroModel = model;
+        backgroundIntroView = view;
+
+        RefreshView();
+    }
+
+    private void RefreshView()
+    {
+        backgroundIntroView.TypeText(textIntroModel.GetCurrentText());
+
+        if (textIntroModel.CanGoPreviousText())
+            backgroundIntroView.EnablePreviousButton();
+        else
+            backgroundIntroView.DisablePreviousButton();
+
+        if (textIntroModel.CanGoNextText())
+            backgroundIntroView.EnableNextButton();
+        else
+            backgroundIntroView.SetNextButtonForConnection();
+    }
+
+    public void ProceedIntro()
+    {
+        if (textIntroModel.CanGoNextText())
+        {
+            textIntroModel.GoNextText();
+            RefreshView();
+        }
+        else
+        {
+            Debug.Log("Fin de l’introduction → lancer la suite du jeu");
+        }
+    }
+
+    public void GoBackIntro()
+    {
+        if (textIntroModel.CanGoPreviousText())
+        {
+            textIntroModel.GoPreviousText();
+            RefreshView();
+        }
+    }
+}
