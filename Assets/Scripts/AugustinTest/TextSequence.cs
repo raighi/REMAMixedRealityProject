42e7fd38139de689fdf23f53db6ea1297f2e88c5
Scarfinos <augustinfaure6@gmail.com>
2026-03-24 16:49:41 +0100

    Adding Avatar movement

--- Assets/Scripts/AugustinTest/TextSequence.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewTextSequence", menuName = "Text/Sequence")]
-public class TextSequence : ScriptableObject
-{
-    [TextArea(4, 15)]
-    public string[] messages;
-}
