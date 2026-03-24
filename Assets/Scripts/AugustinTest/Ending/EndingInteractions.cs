b6a80de41ae2c1e6c5b6dac49e3e828db9e9444e
Scarfinos <augustinfaure6@gmail.com>
2026-03-24 08:50:50 +0100

    adding messages for intro and ending

---
+++ Assets/Scripts/AugustinTest/Ending/EndingInteractions.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingInteractions : MonoBehaviour
+{
+    [Header("UI Component")]
+    public Button nextButton;
+
+    [Header("App Controller")]
+    private EndingController introController;
+
+    public void Initialize(EndingController controller)
+    {
+        introController = controller;
+    }
+
+    private void OnEnable()
+    {
+        nextButton.onClick.AddListener(OnNextPressed);
+    }
+
+    private void OnDisable()
+    {
+        nextButton.onClick.RemoveListener(OnNextPressed);
+    }
+
+    private void OnNextPressed()
+    {
+        introController.ProceedIntro();
+    }
+}
